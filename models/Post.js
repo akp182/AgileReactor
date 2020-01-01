d612273cdff27194b6dcf5bbb18c9287571fbfd4
akp182 <topgunz_akp@outlook.com>
2020-01-01 14:07:01 +0545

    this is like and comment schema

--- models/Post.js
+++ models/Post.js
@@ -16,7 +16,27 @@ const PostSchema = new Schema({
     type: String
   },
 
-
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
   date: {
     type: Date,
     default: Date.now
