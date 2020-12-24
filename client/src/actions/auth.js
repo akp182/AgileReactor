54addeeee6a9c9eed6a58a82ccbcc7f7153b77a1
AG1L1TYx7 <bishworupx7@gmail.com>
2020-12-24 17:10:41 +0545

    import impt file

---
+++ client/src/actions/auth.js
@@ -0,0 +1,15 @@
+import api from '../utils/api';
+import { setAlert } from './alert';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT
+} from './types';
+
+
+// Logout
+export const logout = () => ({ type: LOGOUT });
