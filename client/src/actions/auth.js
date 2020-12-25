dd557f0adbf35ed596be3c4bd3af527dc4c3d466
AG1L1TYx7 <bishworupx7@gmail.com>
2020-12-25 17:11:53 +0545

    create load user

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -10,6 +10,22 @@ import {
   LOGOUT
 } from './types';
 
+// Load User
+export const loadUser = () => async dispatch => {
+  try {
+    const res = await api.get('/auth');
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR
+    });
+  }
+};
+
 
 // Logout
 export const logout = () => ({ type: LOGOUT });
