0edb33b0fa2c12dfcfbfbcd176703135b9b71408
AG1L1TYx7 <bishworupx7@gmail.com>
2020-12-31 17:13:41 +0545

    uploaded login js file

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -49,7 +49,31 @@ export const register = formData => async dispatch => {
   }
 };
 
+// Login User
+export const login = (email, password) => async dispatch => {
+  const body = { email, password };
 
+  try {
+    const res = await api.post('/auth', body);
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    });
+
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: LOGIN_FAIL
+    });
+  }
+};
 
 // Logout
 export const logout = () => ({ type: LOGOUT });
