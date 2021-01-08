c41ee9a1a6c3a25a1701d0bf2f27fb8bf5889519
AG1L1TYx7 <bishworupx7@gmail.com>
2021-01-08 17:22:32 +0545

    add post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -81,3 +81,22 @@ export const deletePost = id => async dispatch => {
   }
 };
 
+// Add post
+export const addPost = formData => async dispatch => {
+  try {
+    const res = await api.post('/posts', formData);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Post Created', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
