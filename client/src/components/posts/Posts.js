e73f8036cc1016bcf35397d2471d4a42ddb74377
bikramoli <bikramoli75@gmail.com>
2021-01-11 19:24:57 +0545

    propstype

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -26,6 +26,13 @@ const Posts = ({ getPosts, post: { posts } }) => {
   );
 };
 
+Posts.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
 
+const mapStateToProps = (state) => ({
+  post: state.post
+});
 
 export default connect(mapStateToProps, { getPosts })(Posts);
