d734b33de2d8817db72d9ac3141df6da95586a02
bikramoli <bikramoli75@gmail.com>
2021-01-12 19:57:32 +0545

    profile about completed

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -16,11 +16,19 @@ const ProfileAbout = ({
         <div className='line' />
       </Fragment>
     )}
-
+    <h2 className='text-primary'>Skill Set</h2>
+    <div className='skills'>
+      {skills.map((skill, index) => (
+        <div key={index} className='p-1'>
+          <i className='fas fa-check' /> {skill}
+        </div>
+      ))}
+    </div>
+  </div>
 );
 
 ProfileAbout.propTypes = {
-      profile: PropTypes.object.isRequired
+  profile: PropTypes.object.isRequired
 };
 
 export default ProfileAbout;
