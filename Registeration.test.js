7b47b1af9d293a7edb743173e8f8318d27963ad4
shusan1 <63546803+shusan1@users.noreply.github.com>
2021-01-13 14:31:54 +0545

    test to create new user

--- Registeration.test.js
+++ Registeration.test.js
@@ -14,3 +14,34 @@ afterAll(async () => {
     await mongoose.connection.close();
 });
 
+describe('User Schema test anything', () => {
+    // the code below is for insert testing
+    it('Add User testing anything', () => {
+        const user = {
+            'email': 'asd@asd.asd',
+            'password': '123123',
+            'name': 'shusan'
+
+
+        };
+
+        return User.create(user)
+            .then((pro_ret) => {
+                expect(pro_ret.email).toEqual('asd@asd.asd');
+            });
+    });
+    // test for update
+    it('to test the update', async () => {
+
+        return User.findOneAndUpdate({ email: 'asd@asd.asd' }, { $set: { email: 'test@gmail.com' } })
+            .then((pp) => {
+                expect(pp.email).toEqual('asd@asd.asd')
+            })
+    });
+    // the code below is for delete testing
+    it('to test the delete product is working or not', async () => {
+        const status = await User.deleteMany();
+        // const status = await User.deleteOne({ _id: "5f19438de3070525782ebac7" });
+        expect(status.ok).toBe(1);
+    });
+})
