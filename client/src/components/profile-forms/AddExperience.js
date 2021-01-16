1ee37c7a5024b3e8833354be71e11151e3a27c1d
bikramoli <bikramoli75@gmail.com>
2021-01-16 20:09:11 +0545

    experience adding form completed

--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -35,62 +35,79 @@ const AddExperience = ({ addExperience, history }) => {
           addExperience(formData, history);
         }}
       >
-        
-        />
-        </div>
-      <div className="form-group">
-        <input
-          type="text"
-          placeholder="Location"
-          name="location"
-          value={location}
-          onChange={onChange}
-        />
-      </div>
-      <div className="form-group">
-        <h4>From Date</h4>
-        <input type="date" name="from" value={from} onChange={onChange} />
-      </div>
-      <div className="form-group">
-        <p>
+        <div className="form-group">
           <input
-            type="checkbox"
-            name="current"
-            checked={current}
-            value={current}
-            onChange={() => {
-              setFormData({ ...formData, current: !current });
-            }}
-          />{' '}
+            type="text"
+            placeholder="* Job Title"
+            name="title"
+            value={title}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* Company"
+            name="company"
+            value={company}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Location"
+            name="location"
+            value={location}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <h4>From Date</h4>
+          <input type="date" name="from" value={from} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <p>
+            <input
+              type="checkbox"
+              name="current"
+              checked={current}
+              value={current}
+              onChange={() => {
+                setFormData({ ...formData, current: !current });
+              }}
+            />{' '}
             Current Job
           </p>
-      </div>
-      <div className="form-group">
-        <h4>To Date</h4>
-        <input
-          type="date"
-          name="to"
-          value={to}
-          onChange={onChange}
-          disabled={current}
-        />
-      </div>
-      <div className="form-group">
-        <textarea
-          name="description"
-          cols="30"
-          rows="5"
-          placeholder="Job Description"
-          value={description}
-          onChange={onChange}
-        />
-      </div>
-      <input type="submit" className="btn btn-primary my-1" />
-      <Link className="btn btn-light my-1" to="/dashboard">
-        Go Back
+        </div>
+        <div className="form-group">
+          <h4>To Date</h4>
+          <input
+            type="date"
+            name="to"
+            value={to}
+            onChange={onChange}
+            disabled={current}
+          />
+        </div>
+        <div className="form-group">
+          <textarea
+            name="description"
+            cols="30"
+            rows="5"
+            placeholder="Job Description"
+            value={description}
+            onChange={onChange}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary my-1" />
+        <Link className="btn btn-light my-1" to="/dashboard">
+          Go Back
         </Link>
       </form>
-    </Fragment >
+    </Fragment>
   );
 };
 
