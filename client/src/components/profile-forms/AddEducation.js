006224003bec913998cb7969369075c5e418be6a
bikramoli <bikramoli75@gmail.com>
2021-01-16 20:06:55 +0545

    add education

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -43,50 +43,77 @@ const AddEducation = ({ addEducation, history }) => {
           addEducation(formData, history);
         }}
       >
-        
-        </div>
-      <div className="form-group">
-        <h4>From Date</h4>
-        <input type="date" name="from" value={from} onChange={onChange} />
-      </div>
-      <div className="form-group">
-        <p>
+        <div className="form-group">
           <input
-            type="checkbox"
-            name="current"
-            checked={current}
-            value={current}
-            onChange={() => setFormData({ ...formData, current: !current })}
-          />{' '}
+            type="text"
+            placeholder="* School or Bootcamp"
+            name="school"
+            value={school}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="* Degree or Certificate"
+            name="degree"
+            value={degree}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Field of Study"
+            name="fieldofstudy"
+            value={fieldofstudy}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <h4>From Date</h4>
+          <input type="date" name="from" value={from} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <p>
+            <input
+              type="checkbox"
+              name="current"
+              checked={current}
+              value={current}
+              onChange={() => setFormData({ ...formData, current: !current })}
+            />{' '}
             Current School
           </p>
-      </div>
-      <div className="form-group">
-        <h4>To Date</h4>
-        <input
-          type="date"
-          name="to"
-          value={to}
-          onChange={onChange}
-          disabled={current}
-        />
-      </div>
-      <div className="form-group">
-        <textarea
-          name="description"
-          cols="30"
-          rows="5"
-          placeholder="Program Description"
-          value={description}
-          onChange={onChange}
-        />
-      </div>
-      <input type="submit" className="btn btn-primary my-1" />
-      <Link className="btn btn-light my-1" to="/dashboard">
-        Go Back
+        </div>
+        <div className="form-group">
+          <h4>To Date</h4>
+          <input
+            type="date"
+            name="to"
+            value={to}
+            onChange={onChange}
+            disabled={current}
+          />
+        </div>
+        <div className="form-group">
+          <textarea
+            name="description"
+            cols="30"
+            rows="5"
+            placeholder="Program Description"
+            value={description}
+            onChange={onChange}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary my-1" />
+        <Link className="btn btn-light my-1" to="/dashboard">
+          Go Back
         </Link>
       </form>
-    </Fragment >
+    </Fragment>
   );
 };
 
