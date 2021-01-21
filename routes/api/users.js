9c74c422c2f873b78339221b5da00656ba779634
akp182 <topgunz_akp@outlook.com>
2021-01-21 15:07:01 +0545

    api for register users

--- routes/api/users.js
+++ routes/api/users.js
@@ -23,7 +23,7 @@ router.post(
     ).isLength({ min: 6 })
   ],
   async (req, res) => {
-    const errors = validationResult(req);
+    const errors = validationResult(req); thisdfosdfisofhisdhf
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
@@ -39,6 +39,48 @@ router.post(
           .json({ errors: [{ msg: 'User already exists' }] });
       }
 
-      
+      const avatar = normalize(
+        gravatar.url(email, {
+          s: '200',
+          r: 'pg',
+          d: 'mm'
+        }),
+        { forceHttps: true }
+      );
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: '5 days' },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
