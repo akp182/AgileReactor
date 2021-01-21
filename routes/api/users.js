cfa36751388d85d9b2663798f7da7abc8698d3d8
akp182 <topgunz_akp@outlook.com>
2021-01-21 15:07:38 +0545

    user api complete

--- routes/api/users.js
+++ routes/api/users.js
@@ -23,7 +23,7 @@ router.post(
     ).isLength({ min: 6 })
   ],
   async (req, res) => {
-    const errors = validationResult(req); thisdfosdfisofhisdhf
+    const errors = validationResult(req); 
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
